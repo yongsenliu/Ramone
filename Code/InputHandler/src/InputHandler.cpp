9fbc5fafd42b25cd810873fbe30ae82fd37db4d1
clevin1 <carl.levin@volvocars.com>
2021-11-15 16:37:49 +0100

    Added libSocketCan and updated InputHandler to send CAN

--- Code/InputHandler/src/InputHandler.cpp
+++ Code/InputHandler/src/InputHandler.cpp
@@ -9,20 +9,20 @@ void inputWindowInit(){
 
 UserInput::UserInput(){
     this->accPedalPos = 0;
-    this->gearLever = 'N';
+    this->gearLeverPos = N;
     this->sensorRunning = true;
     printw("Gear lever and acceleration pedal is up and running. Acceleration pedal position is ");
     printw("%d", accPedalPos);
     printw(" %%");
     printw(" and Gearlever is in ");
-    printw("%c", gearLever);
+    printw("%c", gearLeverPos);
     printw(" position. To accelerate use UP-key and deaccelerate use DOWN-key. To change the gear lever press [p/d/n/r]-keys. To turn off press s-key.\n");
 
 }
 
 void UserInput::PrintSensorValues(){
     std::cout << "The position of the acceleration pedal is: " << accPedalPos << std::endl << "\r";
-    std::cout << "The position of the gear lever is: " << gearLever << std::endl << "\r";
+    std::cout << "The position of the gear lever is: " << GearLever(gearLeverPos) << "\n\r";
 
 }
 
@@ -40,16 +40,16 @@ void UserInput::Sensing(int input){
         }
         break;
     case 112:
-        this->gearLever = 'P';
+        this->gearLeverPos = P;
         break;
     case 100:
-        this->gearLever = 'D';
+        this->gearLeverPos = D;
         break;
     case 110:
-        this->gearLever = 'N';
+        this->gearLeverPos = N;
         break;
     case 114:
-        this->gearLever = 'R';
+        this->gearLeverPos = R;
         break;
     case 115:
         this->sensorRunning = false;
@@ -66,3 +66,17 @@ bool UserInput::IsRunning(){
 int UserInput::getAccPedalPos() {
     return accPedalPos;
 }
+
+GearLever UserInput::getGearLeverPosition(){
+    return gearLeverPos;
+}
+
+void setupCanFrame(scpp::CanFrame & _canFrame, int id, int length){
+    _canFrame.id = 100;
+    _canFrame.len = 2;
+    for(int i =0; i <2; i++){
+        _canFrame.data[i] = 0;
+    }
+}
+
+
