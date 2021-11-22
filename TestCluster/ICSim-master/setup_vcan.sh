2e8dca890b2dc120e7c37bb43f72d188a37e7a2c
samliu613 <samliu613@hotmail.com>
2021-11-22 16:05:53 +0100

    added .sh to init can0 and can1, and install sdl files.

--- TestCluster/ICSim-master/setup_vcan.sh
+++ TestCluster/ICSim-master/setup_vcan.sh
@@ -1,4 +1,11 @@
+sudo apt-get install libsdl2-dev libsdl2-image-dev can-utils  
+
 sudo modprobe can
 sudo modprobe vcan
 sudo ip link add dev vcan0 type vcan
 sudo ip link set up vcan0
+
+sudo ip link add dev vcan1 type vcan
+sudo ip link set up vcan1
+
+./icsim vcan1
