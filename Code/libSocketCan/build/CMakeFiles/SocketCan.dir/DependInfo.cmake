d9ffbb66607acad59233c405cc1236446848885c
anbardar <elmira.anbardar@gmail.com>
2021-11-23 15:52:23 +0100

    Cmake + gtest infrustructure taken from Göte commit and updated gitignore

---
+++ Code/libSocketCan/build/CMakeFiles/SocketCan.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/eanbarda/projects/groupProject/Ramone/Code/libSocketCan/src/socketcan.cpp" "/home/eanbarda/projects/groupProject/Ramone/Code/libSocketCan/build/CMakeFiles/SocketCan.dir/src/socketcan.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
