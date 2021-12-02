06b30609b4d4ebf2a03fc698f88c46ab14d90c2c
samliu613 <samliu613@hotmail.com>
2021-12-02 15:05:39 +0100

    Removed CAN buffer folder and updated emulator control flow diagram.

--- Code/CanBuffer/src/canBuffer.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <thread>
-#include "canBuffer.hpp"
-
-// void CanBuffer::push(const u_int16_t &id, const unsigned char &length, unsigned char data[]) {
-//     CanBufferData buf;
-//     buf
-// }
