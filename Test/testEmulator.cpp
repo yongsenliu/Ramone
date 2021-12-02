cea6ebc601659536de2dca24f60607f7d4b22157
Göte Håkansson <gote.hakansson@bahnhof.se>
2021-12-02 11:07:30 +0100

    Updated testEmulator

--- Test/testEmulator.cpp
+++ Test/testEmulator.cpp
@@ -1,43 +1,7 @@
-#include "../Code/Emulator/include/emulator.hpp"
+#include "emulator.hpp"
 #include <gtest/gtest.h>
 
 Emulator myEmulator;
-
-/*
-// Test method setGaspedalPosition.
-TEST(emulator, setGaspedalPostion) {
-  // Expect that gaspedelposition of 45% ok(true).
-    EXPECT_TRUE(myEmulator.setGaspedalPostion(45));
-
-  //Expect that gaspedelposition of 101% false
-    EXPECT_FALSE(myEmulator.setGaspedalPostion(101));
-
-//Expect that gaspedelposition of -1% false
-    EXPECT_FALSE(myEmulator.setGaspedalPostion(-1));
-
-  //Expect that gaspedelposition of 0% true
-    EXPECT_TRUE(myEmulator.setGaspedalPostion(0));
-
-//Expect that gaspedelposition of 100% true
-    EXPECT_TRUE(myEmulator.setGaspedalPostion(100));
-}
-*/
-
-/*
-// Test method setgearPosition.
-TEST (emulator, setgearPosition){
-  // Expect that gearPosition of N (Neutral) true.
-  EXPECT_TRUE(myEmulator.setgearPosition(N));
-
-  // Expect that gearPosition of R (Reverse) true.
-  EXPECT_TRUE(myEmulator.setgearPosition(R));
-
-  // Expect that gearPosition of D (Drive) true.
-  EXPECT_TRUE(myEmulator.setgearPosition(D));
-
-  // Expect that gearPosition of P (Parking) true.
-  EXPECT_TRUE(myEmulator.setgearPosition(P));
-*/
 
 // Test method ignition.
 TEST (emulator, Ignition){
